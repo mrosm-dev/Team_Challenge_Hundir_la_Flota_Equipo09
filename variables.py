d62f21d53d57d7a7b438059b1b3c8b1b8c6f89d4
itscarmengo <carmengomezgarciaata@gmail.com>
2025-11-14 20:00:55 +0100

    changes

--- variables.py
+++ variables.py
@@ -5,3 +5,16 @@ agua = " "
 barco = "O"
 tocado = "X"
 agua_tiro = "-"
+
+flota = {
+    "Barco_1": 4,  # un barco de 4 casillas
+    "Barco_2A": 3,
+    "Barco_2B": 3,
+    "Barco_3A": 2,
+    "Barco_3B": 2,
+    "Barco_3C": 2,
+    "Barco_4A": 1,
+    "Barco_4B": 1,
+    "Barco_4C": 1,
+    "Barco_4D": 1,
+}
